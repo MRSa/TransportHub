880fd82620a4b8b179f6144318c5886d8092957b
MRSa <mrsa@myad.jp>
2023-05-24 23:58:08 +0900

    とりあえず

--- app/src/main/java/net/osdn/ja/gokigen/transporthub/presentation/theme/Color.kt
+++ app/src/main/java/net/osdn/ja/gokigen/transporthub/presentation/theme/Color.kt
@@ -3,19 +3,28 @@ package net.osdn.ja.gokigen.transporthub.presentation.theme
 import androidx.compose.ui.graphics.Color
 import androidx.wear.compose.material.Colors
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-val Red400 = Color(0xFFCF6679)
+val Blue200 = Color(0xFFAECBFA)
+val Blue300 = Color(0xFF8AB4F8)
+val Yellow200 = Color(0xFFFdE293)
+val Yellow300 = Color(0xFFFDD663)
+val Black = Color(0xFF000000)
+val DarkGrey = Color(0XFF303133)
+val Red400 = Color(0xFFEE675C)
+val White = Color(0xFFFFFFFF)
+val Grey300 = Color(0xFFDADCE0)
+
 
 internal val wearColorPalette: Colors = Colors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200,
-    secondaryVariant = Teal200,
+    primary = Blue200,
+    primaryVariant = Blue300,
+    secondary = Yellow200,
+    secondaryVariant = Yellow300,
     error = Red400,
-    onPrimary = Color.Black,
-    onSecondary = Color.Black,
-    onError = Color.Black
+    surface = DarkGrey,
+    onPrimary = DarkGrey,
+    onSecondary = DarkGrey,
+    onError = Black,
+    onSurfaceVariant = Grey300,
+    onBackground = White,
+    onSurface = White,
 )
--- app/src/main/java/net/osdn/ja/gokigen/transporthub/presentation/ui/DetailScreen.kt
+++ app/src/main/java/net/osdn/ja/gokigen/transporthub/presentation/ui/DetailScreen.kt
@@ -1,32 +1,38 @@
 package net.osdn.ja.gokigen.transporthub.presentation.ui
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import net.osdn.ja.gokigen.transporthub.R
 import net.osdn.ja.gokigen.transporthub.presentation.theme.GokigenComposeAppsTheme
+import net.osdn.ja.gokigen.transporthub.presentation.theme.wearColorPalette
 
 @Composable
 fun DataDetail(navController: NavHostController)
 {
     GokigenComposeAppsTheme {
-        Row {
+        Column {
             DetailScreenTitle(navController)
-/*
-            Text(text = stringResource(id = R.string.app_name),
-                fontSize = 10.sp,
-                modifier = Modifier.padding(start = 32.dp, top = 32.dp , bottom = 0.dp, end = 32.dp)
-                    .fillMaxSize()
-                )
-*/
+            Text(
+                text = stringResource(id = R.string.app_name),
+                color = wearColorPalette.primaryVariant,
+                fontSize = 12.sp,
+            )
+            ShowGokigenPrivacyPolicy()
         }
     }
 }
@@ -40,7 +46,33 @@ fun DetailScreenTitle(navController: NavHostController) {
     ) {
             Text(
                 text = stringResource(id = R.string.app_name),
+                color = wearColorPalette.primary,
                 fontSize = 12.sp,
             )
+    }
+}
+
+@Composable
+fun ShowGokigenPrivacyPolicy()
+{
+    val density = LocalDensity.current
+    val uriHandler = LocalUriHandler.current
+    val openUri = stringResource(R.string.pref_privacy_policy_url)
+    Row(modifier = Modifier.padding(all = 2.dp)) {
+        Spacer(modifier = Modifier.width(2.dp))
+        Column {
+            Text(
+                text = stringResource(R.string.pref_privacy_policy),
+                color = wearColorPalette.primaryVariant,
+                fontSize = with(density) { 12.sp }
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = openUri,
+                color = wearColorPalette.secondaryVariant,
+                modifier = Modifier.clickable(onClick = { uriHandler.openUri(openUri) }),
+                fontSize = with(density) { 10.sp }
+            )
+        }
     }
 }
